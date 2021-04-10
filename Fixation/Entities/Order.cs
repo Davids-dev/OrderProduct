92c4b081529142c5424d9cac72c67d71e7143967
Davids-dev <davisson.assuncao@gmail.com>
2021-04-09 23:01:46 -0300

    Trabalhando com dados do cliente e exibindo o resumo de seu pedido

---
+++ Fixation/Entities/Order.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fixation.Entities.Enums;
+using System.Globalization;
+
+namespace Fixation.Entities
+{
+    class Order
+    {
+        public DateTime Date { get; set; } = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order()
+        {
+        }
+
+        public Order(OrderStatus status, Client client)
+        {
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0.0;
+
+            foreach (OrderItem obj in Items) //para cada item obj em Items 
+            {
+                total += obj.SubTotal(); //vou somar o total + o valor do Item
+            }
+            return total;
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Order Moment: ");
+            sb.Append(Date.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine();
+            sb.Append("Order Status: ");
+            sb.Append(Status);
+            sb.AppendLine();
+            sb.Append("Client: "); /// CONSIGO FAZER BUSCAR OS ATRIBUTOS ATRAVÉS DAS ASSOCIAÇÕES QUE EU TENHO NA MINHA CLASSE
+            sb.Append(Client.Name);
+            sb.Append(" ");
+            sb.Append(Client.Date); /// CONSIGO FAZER BUSCAR OS ATRIBUTOS ATRAVÉS DAS ASSOCIAÇÕES QUE EU TENHO NA MINHA CLASSE
+            sb.Append(" - ");
+            sb.Append(Client.Email); /// CONSIGO FAZER BUSCAR OS ATRIBUTOS ATRAVÉS DAS ASSOCIAÇÕES QUE EU TENHO NA MINHA CLASSE
+            sb.AppendLine(); 
+            sb.AppendLine("Order items: ");
+            sb.AppendLine();
+
+
+
+            //para cada item dentro da minha lista Items
+            foreach (OrderItem item in Items)
+            {
+                sb.Append(item.Product.Name); /// CONSIGO FAZER BUSCAR OS ATRIBUTOS ATRAVÉS DAS ASSOCIAÇÕES QUE EU TENHO NA MINHA CLASSE
+                sb.Append(", ");
+                sb.Append(item.Product.Price.ToString("F2", CultureInfo.InvariantCulture)); 
+                sb.Append(", Quantity: ");
+                sb.Append(item.Quantity);
+                sb.Append(", Subtotal: $");
+                sb.Append(item.Price);
+                sb.AppendLine();
+            }
+
+            sb.Append("Total price: $");
+            sb.Append(Total().ToString("F2", CultureInfo.InvariantCulture));
+            //CORRIGIR O TO STRING TOTAL PRICE, ESTÁ APARCENDO O TOTAL DUAS VEZES
+
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Fixation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Fixation.Entities;
+using Fixation.Entities.Enums;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+namespace Fixation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birth = DateTime.Parse(Console.ReadLine()); //quando quiser ler uma data utilizo o Datetime time = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, birth); //instanciei meu objeto Cliente com suas caracteristicas
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine()); //tipo enumerado, vai receber o status do pedido em string, vai converter para Enum
+            Order order = new Order(status, client); //do meu cliente, ele vai associar o seu nome
+            Console.Write("How many items to this order?" );
+            int qtdeItems = int.Parse(Console.ReadLine());
+
+            OrderItem orderItem = new OrderItem();
+            for (int i = 1; i <= qtdeItems; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product Name: ");
+                string prodName = Console.ReadLine();
+                Console.Write("Product Price: ");
+                double prodPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int prodQtde = int.Parse(Console.ReadLine());
+
+                Product product = new Product(prodName, prodPrice); //instanciei um objeto do tipo Produto recebendo suas caracteristicas
+                orderItem = new OrderItem(prodQtde, prodPrice, product); //instanciei um objeto do tipo OrderItem(itens do pedido) com seus atributos/caracteristicas
+                order.AddItem(orderItem);// adicionei a minha lista de pedidos o meu objeto
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMARY: ");
+            Console.WriteLine(order);
+
+
+            
+
+
+        }
+    }
+}
